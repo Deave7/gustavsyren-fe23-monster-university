e13a7b0366a18d4ff60c9c5c2003c7ea39d7abb3
Gustav Syrén <g.e.syren@gmail.com>
2024-04-21 20:25:53 +0200

    first commit

---
+++ src/pages/Root.tsx
@@ -0,0 +1,13 @@
+function Root() {
+
+    return (
+        <div className="flex justify-center items-center bg-indigo-300 w-screen h-screen font-serif">
+            <div className="flex flex-col items-center border-2 rounded border-black p-5 gap-5">
+                <h1 className="text-7xl">Welcome to Monsters University!</h1>
+                <p className="text-lg">This is a app that lets you see a list of Monsters!</p>  
+            </div>
+        </div>
+    );
+}
+
+export default Root;
