36947506c4581e798e2e10e96ee202e4cbd9c693
Gustav Syrén <g.e.syren@gmail.com>
2024-04-21 21:16:01 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from 'tailwindcss'
+import path from "path"
 
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-  }
+  },
 })
