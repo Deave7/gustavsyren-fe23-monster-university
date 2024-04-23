4e511e997f1cd31691ef582ade4a531a618e4c2c
Gustav Syrén <g.e.syren@gmail.com>
2024-04-23 15:18:09 +0200

    update

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -2,15 +2,11 @@ import  { useCallback, useState } from "react";
 
 // Part 1
 const useToggle = (initialState: boolean = false) => {
-  // Part 2
   const [state, setState] = useState(initialState);
-
-  // Part 3
   const toggle = useCallback(() => setState((state) => !state), []);
 
-  // Part 4
   return [state, toggle] as const;
 };
 
-// Part 5
+
 export default useToggle;
